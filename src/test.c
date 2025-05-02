ed14291e77da6481bd1341e7c101ab8cfcc8a646
John <shirakami.tametoshi@proton.me>
2025-05-02 13:38:20 -0400

    Update test.c

--- src/test.c
+++ src/test.c
@@ -7,10 +7,10 @@ int* x, *y, *z, *a, *b, *c;
 int main() {
   long i = 0;
   while (true) {
-    x = (int*)malloc( (10000*i) * sizeof(int));
-    y = (int*)malloc( (10000*i) * sizeof(int));
-    z = (int*)malloc( (10000*i) * sizeof(int));
-    a = (int*)malloc( (10000*i) * sizeof(int));
+    x = (int*)malloc( (1000000*i) * sizeof(int));
+    y = (int*)malloc( (1000000*i) * sizeof(int));
+    z = (int*)malloc( (1000000*i) * sizeof(int));
+    a = (int*)malloc( (1000000*i) * sizeof(int));
     ++i;
   }
   return 0;
